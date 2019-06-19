02fda123040b15581a8e5f3df6cef6749f0e25b4
Carlos Binderbul <calva.cartablanca@gmail.com>
2019-06-19 18:04:01 -0500

    Login de spotify

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,52 @@
 import React, { Component } from "react";
-
+import { authEndpoint, clientId, redirectUri, scopes } from "../functions/config";
+import hash from "../functions/hash";
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            token: null,
+            item: {
+                album: {
+                    images: [{ url: "" }]
+                },
+                name: "",
+                artists: [{ name: "" }],
+                duration_ms: 0,
+            },
+            is_playing: "Paused",
+            progress_ms: 0
+        };
+        //this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    }
+
+    componentDidMount() {
+        // Set token
+        let _token = hash.access_token;
+        if (_token) {
+            // Set token
+            this.setState({
+                token: _token
+            });
+        }
+    }
 
     render() {
         return (
             <div>
+                <h1>Login Spotify</h1>
+
+                {!this.state.token && (
+                    <a
+                        className="btn"
+                        href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+                    >
+                        Login to Spotify
+                    </a>
+                )}
+
+
                 <h1>Login</h1>
                 {!this.props.auth.isAuthenticated() &&
                     <button className="button" onClick={this.props.auth.login}>Login</button>
---
+++ src/functions/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "d8077fcf73b140768c92577d3d231dee";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
