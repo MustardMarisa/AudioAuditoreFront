263b6ef9c350a28a8cab45bedd3585895c6ed5f5
AntonioMarza <vektor-2.0@live.com>
2019-06-20 21:11:58 -0500

    texto agregado

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,10 +16,42 @@ class Home extends Component {
     render = () => {
         return (
             <div>
-                <h1>Home</h1>
+                
                 <a className="btn" href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}>
                     Login to Spotify
                 </a>
+
+                <body className='home'>
+                    <section className='mianbox'>
+                        <div className="container customer_nav">
+                            <h1 className="display-4">Audio</h1>
+                            <h1 className="display-4">Audtiore</h1>
+                        </div>
+
+                <div className='box_custom'> 
+
+                    <div class="plane main">
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                            <div class="circle"></div>
+                    </div>
+
+                    
+
+
+                </div>        
+                   
+                    
+
+                    </section>
+                </body>
+
+            
+
+                
             </div>
         );
     }
